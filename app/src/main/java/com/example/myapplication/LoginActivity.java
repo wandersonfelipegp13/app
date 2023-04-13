59ad9ca03328cc613d54cc5d1ae66661d16fbf44
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2023-04-13 17:58:42 -0300

    feat: Layout da tela de Cadastro de Usuários

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,14 +2,36 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.myapplication.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private ActivityLoginBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        onClickSignUp();
+
+    }
+
+    private void onClickSignUp() {
+
+        binding.tvSignUpLink.setOnClickListener(view -> {
+
+            Intent intent = new Intent(this, SignUpActivity.class);
+            startActivity(intent);
+
+        });
+
     }
 
 }
---
+++ app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        onClickLogin();
+
+    }
+
+    public void onClickLogin() {
+        binding.tvSignInLink.setOnClickListener(view -> finish());
+    }
+
+}
