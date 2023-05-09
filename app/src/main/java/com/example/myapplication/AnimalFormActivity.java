6e38a9e48cc4afb05479afc57a959ba1b59131d3
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2023-05-08 21:59:37 -0300

    feat: Date Picker no form de animal

--- app/src/main/java/com/example/myapplication/AnimalFormActivity.java
+++ app/src/main/java/com/example/myapplication/AnimalFormActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,7 +12,15 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.example.myapplication.databinding.ActivityAnimalFormBinding;
 import com.example.myapplication.util.AppToast;
+import com.example.myapplication.util.DateValidatorNoFuture;
 import com.example.myapplication.util.ToolbarConfig;
+import com.google.android.material.datepicker.CalendarConstraints;
+import com.google.android.material.datepicker.MaterialDatePicker;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class AnimalFormActivity extends AppCompatActivity {
 
@@ -26,6 +35,60 @@ public class AnimalFormActivity extends AppCompatActivity {
 
         Toolbar toolbar = binding.toolbar;
         ToolbarConfig.config(this, toolbar);
+
+        onClickSelectBirthDate();
+        onClickDeleteBirthDate();
+
+    }
+
+    private void onClickSelectBirthDate() {
+
+        binding.llSelectDate.setOnClickListener(view -> {
+
+            CalendarConstraints calendarConstraints =
+                    new CalendarConstraints.Builder()
+                            .setValidator(new DateValidatorNoFuture())
+                            .build();
+
+            MaterialDatePicker<Long> datePicker =
+                        MaterialDatePicker.Builder.datePicker()
+                                .setTitleText(getString(R.string.animal_birth))
+                                .setInputMode(MaterialDatePicker.INPUT_MODE_TEXT)
+                                .setCalendarConstraints(calendarConstraints)
+                                .build();
+
+
+            datePicker.addOnPositiveButtonClickListener(selection -> {
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTimeInMillis(selection);
+                calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR) + 1);
+
+                DateFormat df = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+                binding.tvDataNasc.setText(df.format(calendar.getTime()));
+
+            });
+
+            datePicker.show(getSupportFragmentManager(), "tag");
+
+        });
+
+    }
+
+    private void onClickDeleteBirthDate() {
+
+        binding.ivDeleteDataNasc.setOnClickListener(view -> {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.animal_birth_delete_question);
+            builder.setPositiveButton(R.string.yes, (dialog, id) -> {
+                binding.tvDataNasc.setText(R.string.animal_birth_not_selected);
+            });
+            builder.setNegativeButton(R.string.no, (dialog, id) -> dialog.dismiss());
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+        });
 
     }
 
